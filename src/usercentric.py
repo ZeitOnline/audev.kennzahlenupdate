c46431bd51454ce7631ff9d613e67eafb0fec624
shmbrg <sebastian.humberg@gmail.com>
2020-06-25 11:51:47 +0200

    add Abonnenten - Paid Services - ang. metric to usercentric.py

--- src/usercentric.py
+++ src/usercentric.py
@@ -111,6 +111,8 @@ def get_data(date_from=api.get_datetime_yesterday(),
             "title": "Anzahl Login SSO"
         }, {
             "title": "Anzahl Digitalabonnenten"
+        }, {
+            "title": "Abonnenten - Paid Services - ang."
         }
         ]}
 
@@ -131,7 +133,8 @@ def get_data(date_from=api.get_datetime_yesterday(),
                  "visits_bestellstrecke", "pis", "pis_schranke_register", "pis_schranke_paid",
                  "pis_pur", "best", "best_zplus_red_marketing", "best_zplus_footer",
                  "best_zplus_gesamt", "best_pur_only", "best_pur_upgrade", "best_pur_kombi",
-                 "reg_sso", "reg_schranke", "login_sso", "sum_abonnenten"]
+                 "reg_sso", "reg_schranke", "login_sso", "sum_abonnenten",
+                 "abonnenten_paid_serv_ang"]
     df.columns = col_names
     df.date = pd.to_datetime(df.date, format="%d.%m.%Y")
 
